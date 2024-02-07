4172f946f3e2068c26cd7c9dcd2bb12213ec8b07
asdbukhari <asadbukhari9895@gmail.com>
2024-02-07 13:44:00 +0500

    update

--- src/consumer-screens/ConsumerArtistDetails/components/cards/ServiceDetiailCard.js
+++ src/consumer-screens/ConsumerArtistDetails/components/cards/ServiceDetiailCard.js
@@ -1,5 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, Text, View, TouchableOpacity } from 'react-native';
 import plus from '../../../../assets/plus.png';
 import minus from '../../../../assets/minus.png';
 import travelIcon from '../../../../assets/travel_brown.png';
@@ -7,89 +8,89 @@ import hostingIcon from '../../../../assets/hosting_brown.png';
 import { styles } from '../styles/ServiceDetiailCard.style';
 import { useDispatch, useSelector } from 'react-redux';
 import { addToCart } from '../../../../redux/actions/commonActions';
-import { TouchableOpacity } from '@gorhom/bottom-sheet';
+
 import { Button } from '../../../../components';
 
 const ServiceDetiailCard = ({ id, name, discount_percentage, amount, discounted_price, is_travelling, is_hosting }) => {
-    const dispatch = useDispatch();
-    const token = useSelector(state => state.auth.token);
-    const calculatePrice = (discount, actualPrice) => {
-        let val = (discount / 100) * actualPrice;
-        return actualPrice - val;
+  const dispatch = useDispatch();
+  const token = useSelector(state => state.auth.token);
+  const calculatePrice = (discount, actualPrice) => {
+    let val = (discount / 100) * actualPrice;
+    return actualPrice - val;
+  };
+  const [cardCartItem, setCardCartItem] = useState({});
+  const cart = useSelector(state => state.common.cart);
+  console.log('this is the id', id);
+  const calculateCartItem = (idItem, cartItem) => {
+    return cartItem.filter(item => item.service_id === idItem)[0];
+  };
+  function handleAddToCart() {
+    console.log('handleadd to the cart press');
+    const cartData = {
+      quantity: 1,
+      service_id: id,
     };
-    const [cardCartItem, setCardCartItem] = useState({});
-    const cart = useSelector(state => state.common.cart);
-    console.log('this is the id', id);
-    const calculateCartItem = (idItem, cartItem) => {
-        return cartItem.filter(item => item.service_id === idItem)[0];
-    };
-    function handleAddToCart() {
-        console.log('handleadd to the cart press');
-        const cartData = {
-            quantity: 1,
-            service_id: id,
-        };
-        dispatch(addToCart(cartData, token));
-    }
-    useEffect(() => {
-        setCardCartItem(calculateCartItem(id, cart));
-    }, []);
-    return (
-        <View style={[styles.container]}>
-            <View style={[styles.flex, styles.flexDirectionRow, styles.justifyBetween, styles.alignItemStart]}>
-                <View>
-                    <View style={[styles.flex, styles.flexDirectionRow, styles.alignItemCenter]}>
-                        <Text style={[styles.heading]}>{name}</Text>
-                        {discount_percentage > 0 && <Text style={[styles.offerContainer]}>{discount_percentage}% Off</Text>}
-                    </View>
-                    <View>
-                        {discount_percentage > 0 ? (
-                            <View style={[styles.flex, styles.flexDirectionRow, styles.alignItemCenter]}>
-                                <Text style={[styles.colorBlue]}>Rs {amount - discounted_price}</Text>
-                                <Text
-                                    style={[styles.colorGray, discount_percentage > 0 && styles.textLilneThrough, styles.marginLeft10]}>
-                                    Rs {amount}
-                                </Text>
-                            </View>
-                        ) : (
-                            <Text style={[styles.colorBlue]}>Rs {amount}</Text>
-                        )}
-                    </View>
-                </View>
-                <View style={[styles.flex, styles.flexDirectionRow, styles.alignItemCenter]}>
-                    {cardCartItem?.id?.length > 0 && (
-                        <View style={[styles.plusBtnContainer]}>
-                            <Image style={[styles.plusIcon]} source={minus} />
-                        </View>
-                    )}
-
-                    {cardCartItem?.id?.length > 0 && (
-                        <Text style={[styles.colorBlack, styles.fontSize10, styles.marginHorizontal5]}>
-                            {cardCartItem?.quantity}
-                        </Text>
-                    )}
-
-                    <TouchableOpacity
-                        onPress={() => {
-                            console.log('hi main click hova hoon');
-                        }}
-                        style={[styles.plusBtnContainer]}>
-                        <Image style={[styles.plusIcon]} source={plus} />
-                    </TouchableOpacity>
-                </View>
-            </View>
-            <View style={[styles.flex, styles.flexDirectionRow, styles.justifyBetween, styles.alignItemCenter]}>
-                <View>
-                    <Text style={[styles.colorGray]}>Takes 40 min</Text>
-                </View>
-                <View style={[styles.flex, styles.flexDirectionRow, styles.justifyBetween, styles.alignItemCenter]}>
-                    {is_travelling && <Image style={[styles.icons, styles.marginLeft10]} source={travelIcon} />}
-                    {is_hosting && <Image style={[styles.icons, styles.marginLeft10]} source={hostingIcon} />}
-                    <Text style={[styles.marginLeft10, styles.link]}>View</Text>
-                </View>
-            </View>
+    dispatch(addToCart(cartData, token));
+  }
+  useEffect(() => {
+    setCardCartItem(calculateCartItem(id, cart));
+  }, []);
+  return (
+    <View style={[styles.container]}>
+      <View style={[styles.flex, styles.flexDirectionRow, styles.justifyBetween, styles.alignItemStart]}>
+        <View>
+          <View style={[styles.flex, styles.flexDirectionRow, styles.alignItemCenter]}>
+            <Text style={[styles.heading]}>{name}</Text>
+            {discount_percentage > 0 && <Text style={[styles.offerContainer]}>{discount_percentage}% Off</Text>}
+          </View>
+          <View>
+            {discount_percentage > 0 ? (
+              <View style={[styles.flex, styles.flexDirectionRow, styles.alignItemCenter]}>
+                <Text style={[styles.colorBlue]}>Rs {amount - discounted_price}</Text>
+                <Text
+                  style={[styles.colorGray, discount_percentage > 0 && styles.textLilneThrough, styles.marginLeft10]}>
+                  Rs {amount}
+                </Text>
+              </View>
+            ) : (
+              <Text style={[styles.colorBlue]}>Rs {amount}</Text>
+            )}
+          </View>
         </View>
-    );
+        <View style={[styles.flex, styles.flexDirectionRow, styles.alignItemCenter]}>
+          {cardCartItem?.id?.length > 0 && (
+            <View style={[styles.plusBtnContainer]}>
+              <Image style={[styles.plusIcon]} source={minus} />
+            </View>
+          )}
+
+          {cardCartItem?.id?.length > 0 && (
+            <Text style={[styles.colorBlack, styles.fontSize10, styles.marginHorizontal5]}>
+              {cardCartItem?.quantity}
+            </Text>
+          )}
+
+          <TouchableOpacity
+            onPress={() => {
+              console.log('hi main click hova hoon');
+            }}
+            style={[styles.plusBtnContainer]}>
+            <Image style={[styles.plusIcon]} source={plus} />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={[styles.flex, styles.flexDirectionRow, styles.justifyBetween, styles.alignItemCenter]}>
+        <View>
+          <Text style={[styles.colorGray]}>Takes 40 min</Text>
+        </View>
+        <View style={[styles.flex, styles.flexDirectionRow, styles.justifyBetween, styles.alignItemCenter]}>
+          {is_travelling && <Image style={[styles.icons, styles.marginLeft10]} source={travelIcon} />}
+          {is_hosting && <Image style={[styles.icons, styles.marginLeft10]} source={hostingIcon} />}
+          <Text style={[styles.marginLeft10, styles.link]}>View</Text>
+        </View>
+      </View>
+    </View>
+  );
 };
 
 export default ServiceDetiailCard;
--- src/consumer-screens/ConsumerArtistDetails/index.js
+++ src/consumer-screens/ConsumerArtistDetails/index.js
@@ -1,235 +1,227 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { View, Text, ScrollView, StyleSheet, ImageBackground, TouchableOpacity, Image } from 'react-native'
-import { Button, Header } from '../../components';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
+import { Button } from '../../components';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { styles } from "./style.js"
+import { styles } from './style.js';
 import { useNavigation } from '@react-navigation/native';
 import ArtistDetailHeader from './components/header/index.js';
 import AvailablityStatus from './components/cards/AvailablityStatus.js';
 import Mood from './components/cards/Mood.js';
 import Socials from './components/cards/Socials.js';
 import PromotionalCard from './components/cards/PromotionalCard.js';
-import face_promo from "../../assets/face_promo.png"
-import girl_hair from "../../assets/girl_hair.png"
+import face_promo from '../../assets/face_promo.png';
+import girl_hair from '../../assets/girl_hair.png';
 import { FlatList } from 'react-native';
-import { heightToDp, widthToDp } from '../../utils/Dimensions.js';
 import NavigationTabs from './components/NavigationTabs.js';
 import ServiceDetiailCard from './components/cards/ServiceDetiailCard.js';
 import Modal from 'react-native-modal';
-import Feather from 'react-native-vector-icons/Feather';
-import { useTheme } from '../../utils/theme.js';
+
 import HostAndTravel from './components/cards/HostAndTravel.js';
-import hosting from "../../assets/hosting_white.png"
-import travel from "../../assets/travel_white.png"
+import hosting from '../../assets/hosting_white.png';
+import travel from '../../assets/travel_white.png';
 import { useDispatch, useSelector } from 'react-redux';
 import { getServices } from '../../redux/actions/commonActions.js';
 const DATA = [
-    {
-        id: '1',
-        title: 'Face Pro Max',
-        price: 2999,
-        imageLink: face_promo
-    },
-    {
-        id: '2',
-        title: 'Buy 3 get one free',
-        price: 2500,
-        imageLink: girl_hair
-    },
+  {
+    id: '1',
+    title: 'Face Pro Max',
+    price: 2999,
+    imageLink: face_promo,
+  },
+  {
+    id: '2',
+    title: 'Buy 3 get one free',
+    price: 2500,
+    imageLink: girl_hair,
+  },
 ];
-const serviceDetailData = [
-    {
-        id: 'c28c63f7-7255-40e7-a874-4ed6e847fd34',
-        title: 'Hair Cut',
-        off: 20,
-        price: 1500,
-        reachTime: '40 mins',
-        travel: true,
-        hosting: true,
-    },
-    {
-        id: 2,
-        title: 'Hydra Facial',
-        off: 0,
-        price: 1500,
-        reachTime: '1-2 hours',
-        travel: true,
-        hosting: true,
-    },
-    {
-        id: 3,
-        title: 'Rebonding',
-        off: 20,
-        price: 15000,
-        reachTime: '2-3 hours',
-        travel: true,
-    },
-    {
-        id: 4,
-        title: 'Special Braids',
-        off: 0,
-        price: 2000,
-        reachTime: '30 mins',
-        hosting: true,
-    },
-];
+
 const ModalData = [
-    {
-        id: '1',
-        icon: travel,
-        title: "Travel to their studio"
-    },
-    {
-        id: '2',
-        icon: hosting,
-        title: "Host them at your place"
-    },
-]
-const theme = useTheme();
+  {
+    id: '1',
+    icon: travel,
+    title: 'Travel to their studio',
+  },
+  {
+    id: '2',
+    icon: hosting,
+    title: 'Host them at your place',
+  },
+];
 
-const ConsumerArtistDetails = (props) => {
-    console.log('this is the params in the artist details', props.route.params.id);
-    const dispatch = useDispatch();
-    const navigation = useNavigation();
-    const cart = useSelector(state => state.common.cart);
-    const artistServices = useSelector(state => state.common.artistServices);
-    const [openModal, setOpenModal] = useState(false);
-    const [navCategory, setNavCategory] = useState("Hair");
-    const [serviceData, setServiceData] = useState([]);
-    const handleCLoseModal = () => {
-        setOpenModal(prev => !prev);
-        console.log('close and open');
+const ConsumerArtistDetails = props => {
+  console.log('this is the params in the artist details', props.route.params.id);
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const cart = useSelector(state => state.common.cart);
+  const artistServices = useSelector(state => state.common.artistServices);
+  const [openModal, setOpenModal] = useState(false);
+  const [navCategory, setNavCategory] = useState('Hair');
+  const [serviceData, setServiceData] = useState([]);
+  const handleCLoseModal = () => {
+    setOpenModal(prev => !prev);
+    console.log('close and open');
+  };
+
+  useEffect(() => {
+    dispatch(getServices(props.route.params.id));
+  }, [props.route.params.i]);
+
+  useEffect(() => {
+    setOpenModal(prev => !prev);
+  }, [cart]);
+
+  useEffect(() => {
+    handleCategoryFilter(artistServices);
+    console.log('change---->', navCategory);
+  }, [navCategory]);
+
+  const calculateCartTotal = cartItem => {
+    let total = 0;
+    for (let i = 0; i < cartItem.length; i++) {
+      const element = cartItem[i];
+      // if (element.discount_price > 0) {
+      //     total += (element.discount_price - element.price)
+      // } else {
+
+      total += element.price * element.quantity;
+      // }
     }
-    useEffect(() => {
-        dispatch(getServices(props.route.params.id))
+    return total;
+  };
+  const handleCategoryFilter = ser => {
+    console.log('hi--->', ser.services['basic services'][0].category_name);
+    setServiceData([]);
+    ser.services['basic services'].filter(data => {
+      console.log(
+        'data.category_name === navCategory',
+        data.category_name,
+        navCategory,
+        data.category_name === navCategory,
+      );
+      if (data.category_name === navCategory) {
+        console.log('data__equal', data.category_services);
+        setServiceData(data.category_services);
+      } else {
+        setServiceData([]);
+      }
+    });
+  };
 
-    }, [props.route.params.i])
-    useEffect(() => {
-        setOpenModal(prev => !prev);
-    }, [cart])
-    useEffect(() => {
-        handleCategoryFilter(artistServices)
-        console.log('change---->', navCategory);
-    }, [navCategory])
-    const calculateCartTotal = (cartItem) => {
-        let total = 0;
-        for (let i = 0; i < cartItem.length; i++) {
-            const element = cartItem[i];
-    // if (element.discount_price > 0) {
-    //     total += (element.discount_price - element.price)
-    // } else {
-
-            total += (element.price * element.quantity)
-            // }
-        }
-        return total;
-    }
-    const handleCategoryFilter = (ser) => {
-        console.log('hi--->', ser.services["basic services"][0].category_name);
-        setServiceData([])
-        ser.services["basic services"].filter(data => {
-            console.log('data.category_name === navCategory', data.category_name, navCategory, data.category_name === navCategory);
-            if (data.category_name === navCategory) {
-                console.log('data__equal', data.category_services);
-                setServiceData(data.category_services);
-            } else {
-                setServiceData([]);
-            }
-        })
-    }
-
-    return (
-        <SafeAreaView style={styles.container} >
-            <ScrollView contentContainerStyle={{ paddingBottom: 90 }}>
-                <View>
-                    <ArtistDetailHeader />
-
-                </View>
-                <View style={[styles.flex, styles.flexDirectionRow, styles.justifyContentBetween, styles.paddingVertical15, styles.paddingHorizontal0]}>
-                    <AvailablityStatus status={artistServices.availability} />
-                    <Mood travel_mood={artistServices.travel_mood} hosting_mood={artistServices.hosting_mood} />
-                    <Socials followers={artistServices?.followers} rating_count={artistServices?.rating_count} rating={artistServices?.rating} />
-                </View>
-                {artistServices.services["promotional services"].length > 0 && <View
-                    style={[styles.paddingleftl0]}
-                >
-
-                    <View style={[styles.marginBottom10]}>
-                        <Text style={[styles.cardHeading]}>Promotional offers</Text>
-                    </View>
-                <ScrollView
-                        horizontal
-                        contentContainerStyle={{ paddingBottom: 10 }}
-                >
-                    <FlatList
-                        horizontal
-                        data={DATA}
-                        renderItem={({ item }) => <PromotionalCard {...item} />}
-                        keyExtractor={item => item.id}
-                    />
-
-                </ScrollView>
-                </View>}
-
-
-
-
-                <View >
-
-                    <View style={[styles.borderBottom1, styles.borderBottomColorLightGray]} >
-                        <NavigationTabs navCategory={navCategory} setNavCategory={setNavCategory} />
-                    </View>
-                    <View style={[styles.paddingleftl0]}>
-                        <FlatList
-
-                            data={serviceData}
-                            renderItem={({ item }) => <ServiceDetiailCard {...item} />}
-                            keyExtractor={item => item.id}
-                        />
-                        <Text>{serviceData.length <= 0 && `No record found`}</Text>
-                    </View>
-                </View>
-                <TouchableOpacity style={[styles.marginTop30]}>
-                    {cart.length > 0 ? <Button onPress={() =>
-                        navigation.navigate('ConsumerHomeStack', {
-                            screen: 'ConsumerCart',
-                        })
-                    }
-                        title={`View your cart Rs ${calculateCartTotal(cart)}`} /> : <View></View>}
-                </TouchableOpacity>
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 90 }}>
+        <View>
+          <ArtistDetailHeader />
+        </View>
+        <View
+          style={[
+            styles.flex,
+            styles.flexDirectionRow,
+            styles.justifyContentBetween,
+            styles.paddingVertical15,
+            styles.paddingHorizontal0,
+          ]}>
+          <AvailablityStatus status={artistServices.availability} />
+          <Mood travel_mood={artistServices.travel_mood} hosting_mood={artistServices.hosting_mood} />
+          <Socials
+            followers={artistServices?.followers}
+            rating_count={artistServices?.rating_count}
+            rating={artistServices?.rating}
+          />
+        </View>
+        {artistServices.services['promotional services'].length > 0 && (
+          <View style={[styles.paddingleftl0]}>
+            <View style={[styles.marginBottom10]}>
+              <Text style={[styles.cardHeading]}>Promotional offers</Text>
+            </View>
+            <ScrollView horizontal contentContainerStyle={{ paddingBottom: 10 }}>
+              <FlatList
+                horizontal
+                data={DATA}
+                renderItem={({ item }) => <PromotionalCard {...item} />}
+                keyExtractor={item => item.id}
+              />
             </ScrollView>
-            <Modal
-                // coverScreen={false}
-                isVisible={openModal}
-                // animationType={'fade'}
-                // transparent={true}
-                style={[styles.flex1, styles.marginTop500, styles.marginHorizontal0, styles.marginBottom0, styles.backGroundWhite, styles.releative, styles.borderRadiusTop]}
-                onSwipeComplete={false}
-                swipeDirection={'down'}
-                onSwipeCancel={() => handleCLoseModal()}
-            >
-                <View style={[styles.heightFull, styles.flex, styles.justifyContentBetween, styles.alignItemsCenter, styles.padding10]}>
-                    <TouchableOpacity
-                        activeOpacity={1}
-                        onPress={() => handleCLoseModal()}
-                    >
-                        <View style={{ width: 60, height: 5, borderRadius: 3, backgroundColor: '#c4c4c4' }}></View>
-                    </TouchableOpacity>
-                    <View style={[styles.flex, styles.flexDirectionRow, styles.justifyContentCenter]}>
-                        <FlatList
-                            contentContainerStyle={[styles.justifyContentCenter, styles.alignItemsCenter, styles.flex, styles.marginLeftAuto]}
-                            data={ModalData}
-                            renderItem={({ item }) => <HostAndTravel {...item} />}
-                            keyExtractor={item => item.id}
-                            horizontal
-                        />
+          </View>
+        )}
 
-                    </View>
-                    <Button title="Add to cart" />
-                </View>
-            </Modal>
-        </SafeAreaView >
-    )
-}
+        <View>
+          <View style={[styles.borderBottom1, styles.borderBottomColorLightGray]}>
+            <NavigationTabs navCategory={navCategory} setNavCategory={setNavCategory} />
+          </View>
+          <View style={[styles.paddingleftl0]}>
+            <FlatList
+              data={serviceData}
+              renderItem={({ item }) => <ServiceDetiailCard {...item} />}
+              keyExtractor={item => item.id}
+            />
+            <Text>{serviceData.length <= 0 && 'No record found'}</Text>
+          </View>
+        </View>
+        <TouchableOpacity style={[styles.marginTop30]}>
+          {cart.length > 0 ? (
+            <Button
+              onPress={() =>
+                navigation.navigate('ConsumerHomeStack', {
+                  screen: 'ConsumerCart',
+                })
+              }
+              title={`View your cart Rs ${calculateCartTotal(cart)}`}
+            />
+          ) : (
+            <View />
+          )}
+        </TouchableOpacity>
+      </ScrollView>
+      <Modal
+        // coverScreen={false}
+        isVisible={openModal}
+        // animationType={'fade'}
+        // transparent={true}
+        style={[
+          styles.flex1,
+          styles.marginTop500,
+          styles.marginHorizontal0,
+          styles.marginBottom0,
+          styles.backGroundWhite,
+          styles.releative,
+          styles.borderRadiusTop,
+        ]}
+        onSwipeComplete={false}
+        swipeDirection={'down'}
+        onSwipeCancel={() => handleCLoseModal()}>
+        <View
+          style={[
+            styles.heightFull,
+            styles.flex,
+            styles.justifyContentBetween,
+            styles.alignItemsCenter,
+            styles.padding10,
+          ]}>
+          <TouchableOpacity activeOpacity={1} onPress={() => handleCLoseModal()}>
+            <View style={{ width: 60, height: 5, borderRadius: 3, backgroundColor: '#c4c4c4' }} />
+          </TouchableOpacity>
+          <View style={[styles.flex, styles.flexDirectionRow, styles.justifyContentCenter]}>
+            <FlatList
+              contentContainerStyle={[
+                styles.justifyContentCenter,
+                styles.alignItemsCenter,
+                styles.flex,
+                styles.marginLeftAuto,
+              ]}
+              data={ModalData}
+              renderItem={({ item }) => <HostAndTravel {...item} />}
+              keyExtractor={item => item.id}
+              horizontal
+            />
+          </View>
+          <Button title="Add to cart" />
+        </View>
+      </Modal>
+    </SafeAreaView>
+  );
+};
 
-export default ConsumerArtistDetails
+export default ConsumerArtistDetails;
